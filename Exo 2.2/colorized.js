d97c66d631d6666f88a6e26a91f373d50a831e93
Antoine <antoine1805@msn.com>
2022-11-28 15:44:12 +0100

    nouvel exo

--- Exo 2.2/colorized.js
+++ Exo 2.2/colorized.js
@@ -1,20 +1,19 @@
 const submit = document.getElementById("submit");
 
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    const input = document.getElementById("input").value;
-    const colorized = document.getElementById("colorized");
-    let j = 1;
-    for (let i = 0; i < input.length; ++i) {
-        const span = document.createElement('span')
-        const char = document.createTextNode(input[i]) 
-        span.appendChild(char);
-        span.classList.add('color' + j);
-        ++j;
-        if(j > 3){
-            j = 1;
-        }
-        colorized.appendChild(span);
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const input = document.getElementById("input").value;
+  const colorized = document.getElementById("colorized");
+  let j = 1;
+  for (let i = 0; i < input.length; ++i) {
+    const span = document.createElement("span");
+    const char = document.createTextNode(input[i]);
+    span.appendChild(char);
+    span.classList.add("color" + j);
+    ++j;
+    if (j > 3) {
+      j = 1;
     }
-
+    colorized.appendChild(span);
+  }
 });
--- Exo 2.3/horloge.js
+++ Exo 2.3/horloge.js
@@ -1,28 +1,22 @@
-
-
-
 function leadingZeros(input) {
-    if (input < 10) {
-        input = input.toString().padStart(2, "0");
-    }
+  if (input < 10) {
+    input = input.toString().padStart(2, "0");
+  }
 
-    return input
+  return input;
 }
-
 
 function startClock() {
-    const date = new Date();
-    const heures = date.getHours();
-    const minutes = date.getMinutes();
-    const secondes = date.getSeconds();
-    document.getElementById("heures").innerHTML = leadingZeros(heures) + ':';
-    document.getElementById("minutes").innerHTML = leadingZeros(minutes) + ':';
-    document.getElementById("secondes").innerHTML = leadingZeros(secondes);
-    console.log(secondes);
+  const date = new Date();
+  const heures = date.getHours();
+  const minutes = date.getMinutes();
+  const secondes = date.getSeconds();
+  document.getElementById("heures").innerHTML = leadingZeros(heures) + ":";
+  document.getElementById("minutes").innerHTML = leadingZeros(minutes) + ":";
+  document.getElementById("secondes").innerHTML = leadingZeros(secondes);
+  console.log(secondes);
 }
 
-document.getElementById("button").addEventListener('click', () => {
-    setInterval(startClock, 1000);
+document.getElementById("button").addEventListener("click", () => {
+  setInterval(startClock, 1000);
 });
-
-
